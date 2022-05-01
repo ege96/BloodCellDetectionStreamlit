b9b8c328da6829fa538dfe97759b119c866da06f
Eric <70232980+pogman96@users.noreply.github.com>
2022-05-01 15:56:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,12 @@ def draw(image:str, labelVals:list, thresh:float):
 
 @st.cache
 def loadModel():
-    model = torch.hub.load("yolov5", "custom", path="data/weights/best.pt", source="local", force_reload=True)
+    import requests
+    weights = "https://github.com/pogman96/BloodCellDetectionStreamlit/raw/main/data/weights/best.pt"
+    r = requests,get(weights)
+    with open("best.pt", "wb") as f:
+        f.write(r.content)
+    model = torch.hub.load("yolov5", "custom", path="best.pt", source="local", force_reload=True)
     print("loaded model")
     return model
 
